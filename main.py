ca5f6cc14b09f96dbe5e5baf353eba3d1b509fc5
RazanGurung <razangurung2147@gmail.com>
2021-03-02 18:45:55 +0545

    Final Code

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import cv2
+import RPi.GPIO as GPIO
+from time import sleep
+import smtplib
 
 thres = 0.45 # Threshold to detect object
 
@@ -35,20 +38,51 @@ def getObjects(img,objects=[]):
                             cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
     return img,objectInfo
 
+def sendEmail():
+    smtpUser = 'gurungrajan90@gmail.com'
+    smtpPass = 'yourpassword'
 
+    toAdd = 'razangurung2147@gmail.com'
+    fromAdd = smtpUser
+
+    subject = "Intruder Detected"
+    header = 'To:' + toAdd + '\n' + 'From:' + fromAdd + '\n' + 'Subject:' + subject
+    body = "Intruder Detected in the farm land"
+
+    print(header + '\n' + body)
+
+    s = smtplib.SMTP('smtp.gmail.com',587)
+    s.ehlo()
+    s.starttls()
+    s.ehlo()
+    s.login(smtpUser,smtpPass)
+    print("login successfull")
+    s.sendmail(fromAdd,toAdd,header + '\n\n' + body)
+
+    s.quit() 
 #to call the function to detect the object
 if __name__=="__main__":
     #video resolution is set to 640 * 480
     cap = cv2.VideoCapture(0)
     cap.set(3,640)
     cap.set(4,480)
+    buzzer = 17
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(buzzer, GPIO.OUT)
     #  cap.set(10,70)
-
     while True:
         success,img = cap.read()
         #main intruder in farms cow, sheep and horse and so on
-        result,objectInfo = getObjects(img,objects=['cow','sheep','horse','elephant'])
+        result,objectInfo = getObjects(img,objects=['cow','sheep','horse','elephant','person'])
         #printing detected object info
         print(objectInfo)
+        if objectInfo:
+            GPIO.output(buzzer, GPIO.HIGH)
+            sleep(0.5)
+            GPIO.output(buzzer, GPIO.LOW)
+            sleep(0.5)
+            sendEmail()
+	
         cv2.imshow("Output",img)
         cv2.waitKey(1)
